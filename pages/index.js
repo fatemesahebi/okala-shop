a80a72b1a65d28cc0e7a8417b571520f4d04ce29
fateme <f.sahebi.fs@gmail.com>
2022-01-28 18:59:26 +0330

    font family setting

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,8 @@
-import axios from "../lib/axios/setup";
 import {useEffect, useState} from "react";
-import {TextField, Button, Box, Typography} from "@mui/material";
-import theme from "../styles/theme";
+import { Box, Typography} from "@mui/material";
 import {
     getBrandProducts,
 } from "../lib/axios/getData";
-import {error} from "next/dist/build/output/log";
 
 const Home = () => {
     const [data, setData] = useState([])
@@ -15,8 +12,8 @@ const Home = () => {
         catch(res => alert(res.status))
     }, [])
     return (<div>
-        <Typography variant={"subtitle1"}>تایید</Typography>
-        <Box>نبیبتب</Box>
+        <Typography  sx={{backgroundColor:'darkGray.main'}} variant={"subtitle1"}>تایید</Typography>
+        <Box sx={{backgroundColor:'red.main',color:'aqua.main',}}>نبیبتب</Box>
 
     </div>)
 
--- styles/theme.js
+++ styles/theme.js
@@ -1,10 +1,63 @@
-import {createTheme} from "@mui/material";
-import * as url from "url";
-import IRANSansNormal from '../public/fonts/woff2/IRANSansWeb.woff2'
+import {createTheme} from '@mui/material/styles';
+import IranSans100 from "../public/fonts/woff2/IRANSansWeb_UltraLight.woff2";
+import IranSans200 from "../public/fonts/woff2/IRANSansWeb_Light.woff2";
+import IranSans300 from "../public/fonts/woff2/IRANSansWeb.woff2";
+import IranSans400 from "../public/fonts/woff2/IRANSansWeb_Medium.woff2";
+import IranSans500 from "../public/fonts/woff2/IRANSansWeb_Bold.woff2";
+import IranSans600 from "../public/fonts/woff2/IRANSansWeb_Black.woff2";
 
 
+// Create a theme instance.
 const theme = createTheme({
-    palette: {
+    direction: 'rtl',
+    components: {
+        MuiCssBaseline: {
+            styleOverrides:
+                {
+                    "@font-face": {
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 100,
+                        src: `url(${IranSans100}) format('woff')`,
+                    },
+                    fallbacks:[{
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 200,
+                        src: `url(${IranSans200}) format('woff')`,
+                    },{
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 300,
+                        src: `url(${IranSans300}) format('woff')`,
+                    },{
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 400,
+                        src: `url(${IranSans400}) format('woff')`,
+                    },{
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        src: `url(${IranSans500}) format('woff')`,
+                    },{
+                        fontFamily: "iranSans",
+                        fontStyle: "normal",
+                        fontWeight: 600,
+                        src: `url(${IranSans600}) format('woff')`,
+                    }],
+
+                }
+        },
+    },
+    typography: {
+        fontFamily: "iranSans",
+    },
+    body:{
+        fontWeight: 400,
+        fontsize:14,
+    }
+    , palette: {
         white:{
             main : '#FFFFFF',
         } ,
@@ -31,29 +84,6 @@ const theme = createTheme({
             main:'rgba(28,27,27,0.82)',
         },
 
-
-        components: {
-            MuiCssBaseline: {
-                styleOverrides:{
-                    body:{
-                        fontFamily: 'IRANSans',
-                    },
-                    "@font-face":{
-                        fontFamily: 'IRANSans',
-                        fontWeight:300,
-                        fontStyle:'normal',
-                        src:`url(${IRANSansNormal}) format(woff2)`
-                    },
-                    // fallback:[
-                    //
-                    // ]
-                }
-            },},
-        typography: {
-            fontFamily: 'IRANSans',
-        },
-        direction: 'rtl',
-    }
-       });
+    }});
 
 export default theme;
