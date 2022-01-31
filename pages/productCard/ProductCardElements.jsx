847f1f7d5fe52b3c44ebd3e1d9dca235705f89d1
mohammad <mohammadmajidi27@gmail.com>
2022-01-31 10:05:14 +0330

    add product card

---
+++ pages/productCard/ProductCardElements.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import {Container, Grid} from "@mui/material";
+import {useStyles} from "./style.productCard";
+import AddIcon from '@mui/icons-material/Add';
+
+const SingleProduct = ({product}) => {
+    const classes = useStyles()
+    return (
+        <Container className={classes.cardGrid} maxWidth='xs'>
+
+            <Grid container spacing={4}>
+                <Grid item>
+                    <Card className={classes.card}>
+                        <CardMedia className={classes.cardMedia}>
+                            <AddIcon className={classes.icon}/>
+                            <img className={classes.image} src={product.image}/>
+                        </CardMedia>
+                        <CardContent className={classes.cardContent}>
+                            <div className={classes.productName}>
+                                <Typography variant='paragraph'>{product.productName} </Typography>
+                            </div>
+                            <div style={{display: 'flex', justifyContent: 'left'}}>
+                                <Typography className={classes.oldPriceCard} gutterBottom variant='paragraph'>{product.oldPrice} </Typography>
+                            </div>
+
+                            <div className={classes.newPrice}>
+                                <Typography className={classes.offPercent}
+                                            variant='paragraph'>{product.pricePercent}</Typography>
+                                <Typography className={classes.cardPrice}
+                                            variant='paragraph'>{product.newPrice}</Typography>
+
+                            </div>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </Container>
+
+    );
+};
+
+export default SingleProduct;
