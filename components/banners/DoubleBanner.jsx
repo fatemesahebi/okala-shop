d6c871c7b9ab2cf92456124bf763e405c01636da
mohammad <mohammadmajidi27@gmail.com>
2022-01-31 22:52:56 +0330

    import banner image statically

--- components/banners/DoubleBanner.jsx
+++ components/banners/DoubleBanner.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
 import {Box, Grid} from '@mui/material'
 import {useStyles} from '../banners/style.banner'
-
+import Image from 'next/image'
+import leftBanner from '../../public/images/banner_2_l.jpg'
+import rightBanner from '../../public/images/banner_2_r.jpg'
 const DoubleBanner = () => {
     const classes = useStyles()
     return (
         <Grid className={classes.bannerSection}container spacing xs={12} sm={12} md={12} lg={12}>
             <Grid item xs={6}>
-                <img src={'https://cdn.okala.com/Media/Index/Slider/389797'}/>
+                <Image src={rightBanner}/>
             </Grid>
             <Grid item xs={6}>
-                <img src={'https://cdn.okala.com/Media/Index/Slider/389798'}/>
+                <Image src={leftBanner}/>
             </Grid>
         </Grid>
     );
