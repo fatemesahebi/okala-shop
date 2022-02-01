618ab0ded949b541da21efac4ce4284c0eff2c2c
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-01 04:30:02 -0800

    minor changes

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,20 @@
 import {useEffect, useState} from "react";
-import { Box, Typography} from "@mui/material";
+import {Box, Typography} from "@mui/material";
 import ListBrands from "../Components/BrandsIconPart/ListBrands";
 import {
     getBrandProducts,
 } from "../lib/axios/getData";
 import HomePage from "../HomePage/homePage";
+
 const Home = () => {
     const [data, setData] = useState([])
     useEffect(() => {
-        getBrandProducts('رامک').
-        then(data => setData(data.products)).
-        catch(res => alert(res.status))
+        getBrandProducts('رامک').then(data => setData(data.products)).catch(res => alert(res.status))
     }, [])
-    return (<div>
-        <Typography  sx={{backgroundColor:'darkGray.main'}} variant={"subtitle1"}>تایید</Typography>
-        <Box sx={{backgroundColor:'red.main',color:'aqua.main',}}>نبیبتب</Box>
-        <ListBrands/>
-        {/*<Typography  sx={{backgroundColor:'darkGray.main'}} variant={"subtitle1"}>تایید</Typography>*/}
-        {/*<Box sx={{backgroundColor:'red.main',color:'aqua.main',}}>نبیبتب</Box>*/}
-<HomePage/>
-    </div>)
+    return (
+        <div>
+            <HomePage/>
+        </div>)
 
 };
 
