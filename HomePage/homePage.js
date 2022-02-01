65c883c9863b90aa043865b7b14c569bec86b97d
mohammad <mohammadmajidi27@gmail.com>
2022-02-01 19:49:41 +0330

    add banner slider to home page

--- HomePage/homePage.js
+++ HomePage/homePage.js
@@ -1,8 +1,9 @@
-import {Header, DailyOffer,FourthBanner,DoubleBanner,ListBrands,ListMiddleNavbar, FooterContainer} from "../components";
+import {Header,BannerSlider, DailyOffer,FourthBanner,DoubleBanner,ListBrands,ListMiddleNavbar, FooterContainer} from "../components";
 function HomePage(){
     return(<div>
         <Header/>
         <ListMiddleNavbar/>
+        <BannerSlider/>
         <DailyOffer/>
         <FourthBanner/>
         <DoubleBanner/>
