89a89e5fbfd067a42f9db3c8c5b23d24be7dcdac
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-01 18:26:01 +0330

    complete responsive middle navbar V.1 :))

--- Components/middleNavbar/ListMiddleNavbar.js
+++ Components/middleNavbar/ListMiddleNavbar.js
@@ -6,25 +6,24 @@ import "swiper/css";
 
 
 const productList = [
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389670&w=384&q=75",text: "پوشاک"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389671&w=384&q=75",text: "مادر و کودک"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389669&w=384&q=75",text: "لوازم التحریر و اداری"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389666&w=384&q=75",text: "ابزار و ملزومات"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389667&w=384&q=75",text: "میوه و سبزیجات"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F390614&w=384&q=75" ,text: "آرایشی بهداشتی"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389665&w=384&q=75",text: "خواربار"},
-    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F390615&w=384&q=75",text: "نوشیدنی و دمنوش ها"},
+
     {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F390613&w=384&q=75",text: "مواد غذایی"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F390615&w=384&q=75",text: "نوشیدنی و دمنوش ها"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389665&w=384&q=75",text: "خواربار"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F390614&w=384&q=75" ,text: "آرایشی بهداشتی"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389667&w=384&q=75",text: "میوه و سبزیجات"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389666&w=384&q=75",text: "ابزار و ملزومات"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389669&w=384&q=75",text: "لوازم التحریر و اداری"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389671&w=384&q=75",text: "مادر و کودک"},
+    {image: "https://new.okala.com/_next/image?url=https%3A%2F%2Fcdn.okala.com%2FMedia%2FIndex%2FCategory%2F389670&w=384&q=75",text: "پوشاک"},
+
 ]
 function ListMiddleNavbar(){
     return(
-        <div style={{ width: '100%',fontSize:".5rem" }}>
+        <div style={{ width: '100%',fontSize:"1rem",fontWeight:"bold" }}>
             <Swiper
                 slidesPerView={3}
                 spaceBetween={2}
-                pagination={{
-                    clickable: true,
-                }}
                 breakpoints={{
                     300: {
                         slidesPerView: 3,
@@ -47,13 +46,17 @@ function ListMiddleNavbar(){
                         spaceBetween: 2,
                     },
                     1280: {
+                        slidesPerView: 8.9,
+                        spaceBetween: 1,
+                    },
+                    1800: {
                         slidesPerView: 9,
                         spaceBetween: 1,
                     },
                 }}
             >
 
-                    {productList.map((item,index) => (<SwiperSlide><MiddleNavbar key={index} imagesProducts={item.image} textProducts={item.text}/> </SwiperSlide>))}
+                {productList.map((item,index) => (<SwiperSlide><MiddleNavbar key={index} imagesProducts={item.image} textProducts={item.text}/> </SwiperSlide>))}
             </Swiper>
         </div>
     )
