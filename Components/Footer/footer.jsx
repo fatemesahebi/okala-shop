74b45c64b697daa26a6941d40bac9f153f492559
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-01 07:12:53 -0800

    added the footer files (original branch corrupted)

---
+++ Components/Footer/footer.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import {Box,Container } from '@mui/material';
+import Row from './index';
+import Features from './features';
+import Application from './application';
+import CustomizedAccordions from "./accordion";
+import {BottomRow} from "./bottomrow";
+
+
+export function FooterContainer() {
+    return (
+        <footer style={{backgroundColor:'#FAFAFA',}}>
+            <Box sx={{backgroundColor:' #F0F0F0', marginBottom:'.125rem'
+            }} >
+                <CustomizedAccordions sx={{backgroundColor:'#F0F0F0'}}/>
+            </Box>
+            <Container maxWidth="xl" sx={{backgroundColor:'#F0F0F0'}}>
+                <Row>
+                    <Features sx={{marginTop:'1rem'}}/>
+                </Row>
+                <Application/>
+                <BottomRow/>
+            </Container>
+        </footer>
+    )
+}
