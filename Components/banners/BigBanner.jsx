28cf7e9eb84ab7a61548f9c11721476850973919
mohammad <mohammadmajidi27@gmail.com>
2022-02-03 12:22:15 +0330

    add border radious to big banner with emotion

--- Components/banners/BigBanner.jsx
+++ Components/banners/BigBanner.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 import Image from 'next/image'
 import {Box, Grid} from '@mui/material'
 import bigBanner from '../../public/images/okalabussines.jpg'
+import styled from "@emotion/styled";
+const StyledImage = styled.img({
+    borderRadius:'12px'
+})
 const BigBanner = () => {
     return (
         <div>
             <Grid container >
                 <Grid item xs={12} container style={{justifyContent: 'center'}}>
-                    <Image style={{borderRadius: '12px'}} src={bigBanner}/>
+                    <StyledImage src={'/images/okalabussines.jpg'}/>
                 </Grid>
             </Grid>
         </div>
