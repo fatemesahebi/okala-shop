fe7f71a20f1c0b36d2255d4a09fb71f009daf989
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-03 07:56:13 -0800

    minor changes

---
+++ components/HeaderLower/HeaderCategories/Subcategories/CategoryBox/CategoryBox.jsx
@@ -0,0 +1,10 @@
+import {Box} from "@mui/material";
+
+const CategoryBox = () => {
+    return (
+        <Box>
+        </Box>
+    )
+}
+
+export default CategoryBox
--- components/HeaderLower/HeaderCategories/Subcategories/Subcategories.jsx
+++ components/HeaderLower/HeaderCategories/Subcategories/Subcategories.jsx
@@ -1,9 +1,22 @@
-import {Box} from "@mui/material";
+import {Box, Typography} from "@mui/material";
+import CategoryBox from "./CategoryBox/CategoryBox";
 
 const Subcategories = () => {
     return (
-        <Box>
-
+        <Box sx={{
+            width: "68rem",
+            display: "flex",
+            flexWrap: "wrap",
+        }}>
+            <Box sx={{
+                display: "flex",
+                flexDirection: "column",
+                flexWrap: "wrap",
+                width: "20rem",
+                height: "31.5rem"
+            }}>
+                <CategoryBox/>
+            </Box>
         </Box>
     )
 }
