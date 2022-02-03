6a102839fac531bfef5c10516512680cafa4d621
mohammad <mohammadmajidi27@gmail.com>
2022-02-03 20:50:11 +0330

    edit product card

--- components/productCard/ProductCardData.js
+++ components/productCard/ProductCardData.js
@@ -1,3 +1,3 @@
-//
-// export const productData=[
-//     {id:1,image:"https://cdn.okala.com/Media/Index/Product/390723",productName:"عطر مردانه ۱۰۰ میلی لیتری sauvage dior پرستیژ",oldPrice:"۲,۲۳۵,۰۰۰  ",newPrice:"۱,۷۷۸,۰۰۰ ریال",pricePercent:"۲۰%"}]
+
+export const productData=[
+    {id:1,image:"https://cdn.okala.com/Media/Index/Product/390723",productName:"عطر مردانه 100 میلی لیتری sauvage dior پرستیژ",price:'2,235,000',priceOffer:'1,778,000 ',offPercent:'20'}]
