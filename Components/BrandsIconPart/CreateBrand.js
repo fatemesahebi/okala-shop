9173144b7e272fed24d9bfc68ad53a1e09d6453d
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-03 23:11:26 +0330

    change is brands paddingtop

--- Components/BrandsIconPart/CreateBrand.js
+++ Components/BrandsIconPart/CreateBrand.js
@@ -7,6 +7,7 @@ function CreateBrand({imageBrand,amongs}){
             <Stack
                 direction="row-reverse"
                 // spacing={2}
+                paddingTop={2}
                 sx={{width:"3rem",height:"3rem"}}
             >
                 <img style={{width:"50px",height:"50px"}} src={imageBrand}/>
