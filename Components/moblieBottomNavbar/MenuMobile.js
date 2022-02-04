f917c85831d84c96b8cbcb88084608f4bce077c4
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-04 14:13:37 +0330

    create grid

--- Components/moblieBottomNavbar/MenuMobile.js
+++ Components/moblieBottomNavbar/MenuMobile.js
@@ -1,31 +1,54 @@
 import * as React from 'react';
 import home from "./../../public/SVG/home.svg"
-import profile from "./../../public/SVG/profile.svg"
-import cart from "./../../public/SVG/shopBascket.svg"
 import search from "./../../public/SVG/search.svg"
+import cart from "./../../public/SVG/buy.svg";
+import profile from "./../../public/SVG/profile.svg"
 import grouping from "./../../public/SVG/grouping.svg"
-import {Stack} from "@mui/material";
+import {Grid, Stack,Typography} from "@mui/material";
 import MenuItem from "./MenuItem";
 import {Button} from "@mui/material";
+import Image from "next/image"
 
-const listItem = [
-    {image: home, title: "خانه"},
-    {image: search, title: "جستجو"},
-    {image: cart, title: ""},
-    {image: grouping, title: "دسته بندی"},
-    {image: profile , title: "پروفایل"},
-]
 
 
 export default function MenuMobile() {
     return (
-        <React.Fragment>
-            <Stack sx={{ display: 'flex',flexDirection:"row", textAlign: 'center',width:"960px" }}>
-                {listItem.map((item,index) => (<Button><MenuItem key={index} iconMenu={item.image} textMenu={item.title}/> </Button>))}
-                {/*/!*<SvgIcon>{home}</SvgIcon>*!/*/}
-                {/*<Typography>home</Typography>*/}
-            </Stack>
 
-        </React.Fragment>
+            <Grid spacing={8}>
+                <Button>
+                <Grid item sx={4}>
+                  <Image src={home}/>
+                    <Typography style={{color:"#888a89",fontSize:".625rem"}}>خانه</Typography>
+                </Grid>
+                </Button>
+
+                <Button>
+                    <Grid item sx={4}>
+                    <Image src={search}/>
+                        <Typography style={{color:"#888a89",fontSize:".625rem"}}>جستجو</Typography>
+                </Grid>
+                </Button>
+
+                   <Button sx={{border:"2px solid red", borderRadius:"80%"}}>
+                       <Image src={cart}/>
+                   </Button>
+
+                    <Button>
+                        <Grid item sx={4}>
+                        <Image src={grouping}/>
+                        <Typography style={{color:"#888a89",fontSize:".625rem"}}>دسته بندی</Typography>
+                        </Grid>
+                    </Button>
+
+                    <Button>
+                        <Grid item sx={4}>
+                    <Image src={profile}/>
+                    <Typography style={{color:"#888a89",fontSize:".625rem"}}>پروفایل</Typography>
+                        </Grid>
+                    </Button>
+
+
+            </Grid>
+
     );
 }
