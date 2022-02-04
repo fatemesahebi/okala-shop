32d6c7fc81410ea009ead15fb4d96bdc8bded684
mohammad <mohammadmajidi27@gmail.com>
2022-02-04 15:48:44 +0330

    edit okalablog

--- Components/okakaBlog/OkalaBlog.jsx
+++ Components/okakaBlog/OkalaBlog.jsx
@@ -1,7 +1,7 @@
 import {okalaBlog} from "../../lib/mirage/dataOKB";
 import {useState} from "react";
 import OkalaBlogElement from "./OkalaBlogElement";
-import {Box, Container} from "@mui/material";
+import {Box, Container, Grid, Stack} from "@mui/material";
 import Typography from "@mui/material/Typography";
 import {Swiper, SwiperSlide} from "swiper/react";
 import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutlined';
@@ -9,21 +9,18 @@ import ArrowBackIosNewOutlinedIcon from '@mui/icons-material/ArrowBackIosNewOutl
 const OkalaBlog = () => {
     return (
         <div style={{width: '1310px', marginTop: '50px', marginRight: '50px'}}>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'space-between',
-                alignContent: 'center',
-                alignItems: 'center',
-                justifyItems: 'center',
-            }}>
-                <Typography>اُکالا بلاگ</Typography>
-                <Typography>مشاهده همه<ArrowBackIosNewOutlinedIcon/></Typography>
-            </div>
+            <Stack>
+                <Grid container sx={{display: 'flex', justifyContent: 'space-between'}}>
+                    <Grid  container sx={{display: 'flex', justifyContent: 'flex-start',alignContent:'center',alignItems:'center',justifyItems:'center'}}><Typography>اُکالا بلاگ</Typography></Grid>
+                        <Grid ><Typography>مشاهده همه</Typography></Grid>
+                        <Grid ><Typography><ArrowBackIosNewOutlinedIcon/></Typography></Grid>
+                </Grid>
+            </Stack>
 
             <Box>
                 <Swiper style={{marginRight: '-22px'}} slidesPerView={4} initialSlide='0'>
                     {okalaBlog.map(blogItem => <SwiperSlide><OkalaBlogElement key={blogItem.id}
-                                                                           blogItem={blogItem}/></SwiperSlide>)}
+                                                                              blogItem={blogItem}/></SwiperSlide>)}
                 </Swiper>
 
             </Box>
