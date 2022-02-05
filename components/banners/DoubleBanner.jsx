2b86b0bad551761020e99f27e467c10d8536088e
mohammad <mohammadmajidi27@gmail.com>
2022-02-05 17:45:28 +0330

    edit banners

--- components/banners/DoubleBanner.jsx
+++ components/banners/DoubleBanner.jsx
@@ -8,7 +8,7 @@ const DoubleBanner = () => {
 
     return (
         <Box   sx={{width:'83vw',marginRight:'8vw'}}>
-            <Grid container >
+            <Grid container  spacing={1} >
                 <Grid item xs={6} container >
                     <Image className={'banner'}  src={rightBanner}/>
                 </Grid>
