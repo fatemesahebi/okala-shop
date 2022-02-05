0abc8516fa8b84a84cf2dae95b06f6e4cfc8143f
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-05 22:55:16 +0330

    add menumobilee

--- Components/MoblieBottomNavbar/MenuMobilee.js
+++ Components/MoblieBottomNavbar/MenuMobilee.js
@@ -9,41 +9,41 @@ import {Button} from "@mui/material";
 import Image from "next/image"
 
 
-export default function MenuMobile() {
+export default function MenuMobilee() {
     return (
         <Box item xs={1} sx={{display: {lg:'none', md:'none'},justifyContent:"space-between",flexShrink:"1",width:"100vw",position:"fixed",bottom:"0",boxShadow:"rgba(0, 0, 0, 0.24) 0px 3px 8px;"}}  >
             <Stack sx={{display:"flex",flexDirection:"row",justifyContent:"space-between",flexShrink:"1"}}>
                 <Button>
-                <Stack>
-                  <Image src={home}/>
-                    <Typography style={{color:"#888a89",fontSize:".625rem"}}>خانه</Typography>
-                </Stack>
+                    <Stack>
+                        <Image src={home}/>
+                        <Typography style={{color:"#888a89",fontSize:".625rem"}}>خانه</Typography>
+                    </Stack>
                 </Button>
 
                 <Button>
-                <Stack>
-                    <Image src={search}/>
+                    <Stack>
+                        <Image src={search}/>
                         <Typography style={{color:"#888a89",fontSize:".625rem"}}>جستجو</Typography>
-                </Stack>
+                    </Stack>
                 </Button>
 
-                   <Button sx={{boxShadow:"rgba(0, 0, 0, 0.35) 0px 5px 15px", borderRadius:"50%"}}>
-                       <Image src={cart}/>
-                   </Button>
+                <Button sx={{boxShadow:"rgba(0, 0, 0, 0.35) 0px 5px 15px", borderRadius:"50%"}}>
+                    <Image src={cart}/>
+                </Button>
 
-                    <Button>
-                        <Stack>
+                <Button>
+                    <Stack>
                         <Image src={grouping}/>
                         <Typography style={{color:"#888a89",fontSize:".625rem"}}>دسته بندی</Typography>
-                        </Stack>
-                    </Button>
+                    </Stack>
+                </Button>
 
-                    <Button>
-                        <Stack>
-                    <Image src={profile}/>
-                    <Typography style={{color:"#888a89",fontSize:".625rem"}}>پروفایل</Typography>
-                        </Stack>
-                    </Button>
+                <Button>
+                    <Stack>
+                        <Image src={profile}/>
+                        <Typography style={{color:"#888a89",fontSize:".625rem"}}>پروفایل</Typography>
+                    </Stack>
+                </Button>
 
 
             </Stack>
