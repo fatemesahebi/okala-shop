686165bae2fefc0b23866df45f4deafc13ae9e99
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-05 01:58:28 -0800

    minor changes

---
+++ Components/HeaderLower/HeaderCategories/Subcategories/CategoryBox/CategoryBox.jsx
@@ -0,0 +1,39 @@
+import {Box, Typography} from "@mui/material";
+
+const CategoryBox = ({categories}) => {
+    return (
+        <>
+            <Box sx={{
+                padding: "1rem",
+                display: "inline-flex",
+                gap: ".5rem"
+            }}>
+                <Box sx={{
+                    border: "2px solid rgba(240,20,54,0.4)",
+                    borderRadius: "4px"
+                }}>
+                </Box>
+                <Typography sx={{
+                    color: "black !important",
+                    fontSize: "0.9rem",
+                    fontWeight: "bold"
+                }}>
+                    {categories.title}
+                </Typography>
+            </Box>
+            {categories.items3.map(item => (
+                <Typography sx={{
+                    paddingRight: "2rem",
+                    color: "black !important",
+                    fontSize: "0.9rem",
+
+                }}>
+                    {item.title}
+                </Typography>
+            ))}
+
+        </>
+    )
+}
+
+export default CategoryBox
