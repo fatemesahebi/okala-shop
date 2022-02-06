52d871c046bf7cd141701a0f33ee5b75da35ccbf
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-06 12:00:53 -0800

    some responsive changes

--- Components/HeaderFooterProvider/HeaderFooterProvider.jsx
+++ Components/HeaderFooterProvider/HeaderFooterProvider.jsx
@@ -1,16 +1,15 @@
 import {Header, FooterContainer} from "../index";
-import {Box} from "@material-ui/core";
+import {Box} from "@mui/material";
 
 const HeaderFooterProvider = ({children}) => {
     return (
-        <>
+        <div>
             <Header/>
-            <Box height={"11rem"}>
-
+            <Box height={{md:"11rem", xs: "0"}}>
             </Box>
             {children}
             <FooterContainer/>
-        </>
+        </div>
     )
 }
 
