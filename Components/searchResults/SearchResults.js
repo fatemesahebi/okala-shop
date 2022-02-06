e8f5c33067127fd5fe7a7140195d1e5a3c0c255f
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-06 12:57:27 +0330

    add style SearchResults

--- Components/searchResults/SearchResults.js
+++ Components/searchResults/SearchResults.js
@@ -4,12 +4,18 @@ import AccordionSummary from '@mui/material/AccordionSummary';
 import AccordionDetails from '@mui/material/AccordionDetails';
 import Typography from '@mui/material/Typography';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import {Input, TextField} from "@mui/material";
+import search from "./../../public/SVG/search.svg"
+import Image from "next/image";
+import Paper from '@mui/material/Paper';
+import InputBase from '@mui/material/InputBase';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import {Box} from "@mui/material";
 
 export default function SearchResults() {
     return (
-        <div>
-            <Accordion sx={{width:"20rem"}}>
+        <Box sx={{padding:"1rem"}}>
+            <Accordion sx={{width:"20rem",boxShadow:"rgba(0, 0, 0, 0.04) 0px 3px 5px",borderRadius:"1rem"}}>
                 <AccordionSummary
                     expandIcon={<ExpandMoreIcon />}
                     aria-controls="panel1a-content"
@@ -18,14 +24,21 @@ export default function SearchResults() {
                     <Typography sx={{borderRight:"4px solid rgba(124, 200, 204, 1)",height:"1rem",top:".5rem",paddingRight:".41rem",right:"0",fontWeight:"bolder"}}> جستجو در نتایج</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    {/*<Typography>*/}
-                    {/*    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse*/}
-                    {/*    malesuada lacus ex, sit amet blandit leo lobortis eget.*/}
-                    {/*</Typography>*/}
-                    <TextField id="outlined-basic" variant="outlined" placeholder="نام کالا یا برند ..." style={{borderRadius:"10px",width:"18rem"}}/>
+                    <Paper
+                        component="form"
+                        sx={{ p: '.55rem .7rem', display: 'flex', alignItems: 'center', width: "18rem" , borderRadius:".43rem",border:"1px solid rgba(175, 175, 175, 1)",boxShadow:"0",fontSize:".875rem"}}
+                    >
+                        <Image src={search}/>
+
+                        <InputBase
+                            sx={{ ml: 1, flex: 1 }}
+                            placeholder="نام کالا یا برند ..."
+                            inputProps={{ 'aria-label': 'search okala' }}
+                        />
+                    </Paper>
                 </AccordionDetails>
             </Accordion>
 
-        </div>
+        </Box>
     );
 }
