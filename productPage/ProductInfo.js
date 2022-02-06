4fa7da4921b13209f83afe5ed5cb78b3ffe5dc30
fateme <f.sahebi.fs@gmail.com>
2022-02-06 20:11:51 +0330

    setting folders of product page

---
+++ productPage/ProductInfo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ProductInfoMob from "./ProductInfoMob";
+const ProductInfo = () => {
+    return (
+        <div>
+            <ProductInfoMob/>
+            <ProductInfoDesk/>
+        </div>
+    );
+};
+
+export default ProductInfo;
---
+++ productPage/ProductInfoMob.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Product from "./mobile/mobileproductpage"
+const ProductInfoMob = () => {
+    return (
+        <div>
+            <Product/>
+        </div>
+    );
+};
+
+export default ProductInfoMob;
