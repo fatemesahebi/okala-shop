542cf25a3b70d572a64d1a07d6a3b32a82485150
Mohammadreza Saffari <m_r_saf@yahoo.com>
2022-02-14 05:32:29 -0800

    fixed the dynamic issue (axios request was missing a "/"... )

--- pages/products/[productId]/[productName].js
+++ pages/products/[productId]/[productName].js
@@ -1,31 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ProductPage from "../../../productPage/ProductPage";
 import ProductMobile from "../../../productPage/mobile/ProductMobile"
 import {HeaderFooterProvider} from "../../../Components";
-import {useRouter} from "next/router";
-import Box from "@mui/material/Box";
-import {getProduct} from "../../../lib/axios/getData";
 
-const Products = () => {
-    const router = useRouter()
+const ProductPageInformation = () => {
     return (
-
         <HeaderFooterProvider>
-            <Box>{router.query.productId}</Box>
             <ProductMobile productId={1}/>
             <ProductPage productId={1}/>
         </HeaderFooterProvider>
     );
 };
 
-export default Products
+export default ProductPageInformation;
 
-// export async function getStaticPaths() {
-//
-//     return {
-//         paths: [
-//             {params: {productId: "1" , productName: ""}}
-//         ],
-//         fallback: 'blocking'
-//     };
-// }
+export async function getStaticPaths() {
+
+    return {
+        paths: [
+            {params: {productId: 'food-stuff' , productName: 'food-stuff'}}
+        ],
+        fallback: 'blocking'
+    };
+}
+
+export async function getStaticProps(context) {
+    console.log(context)
+    let ProductList = []
+
+    return {
+        props: {ProductList}, // will be passed to the page component as props
+    }
+}
