720c0bda5759d2486db2fc1b5a0ef9cc943889e5
zahra nuori sorkhani <zahranuori.1375@gmail.com>
2022-02-15 04:41:20 +0330

    add number product in basket

--- Components/MoblieBottomNavbar/MenuMobile.js
+++ Components/MoblieBottomNavbar/MenuMobile.js
@@ -122,7 +122,7 @@ export default function MenuMobile() {
 
                                     }}>{PN.convertEnToPe(totalOrder)}</Typography>
                                 }
-                                <Image src={shopBascket}/>
+                                <Image src={cart}/>
 
                             </IconHeader>
 
